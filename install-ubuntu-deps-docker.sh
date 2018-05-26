de47dd7bbd3209d0a05a138f75fc4d164ce55bdb
ImperatorS79 <fevrier.simon@gmail.com>
2018-05-26 12:57:05 +0200

    Update install-ubuntu-deps-docker.sh

--- install-ubuntu-deps-docker.sh
+++ install-ubuntu-deps-docker.sh
@@ -7,6 +7,7 @@ docker exec -t ubuntu-18-04 bash -c "apt-get -qq update;
                                      apt-get install -y bison;
 				     apt-get install -y pkg-config;
 				     apt-get install -y libxcb-keysyms1-dev;
+				     apt-get install -y libxcb-util0-dev;
                                      apt-get install -y libvulkan-dev;
 				     wget https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/spirv-headers/1.3-1/spirv-headers_1.3.orig.tar.gz ;
 				     tar -zxvf spirv-headers_1.3.orig.tar.gz;
