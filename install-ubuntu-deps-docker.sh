d95641b53d4a31d677d201604eae0f0a7d3ca592
ImperatorS79 <fevrier.simon@gmail.com>
2018-05-26 12:44:43 +0200

    Fail 2

--- install-ubuntu-deps-docker.sh
+++ install-ubuntu-deps-docker.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 docker exec -t ubuntu-18-04 bash -c "apt-get -qq update;
-                                     apt-get install -y build-essentials;
+                                     apt-get install -y build-essential;
                                      apt-get install -y tree;
                                      apt-get install -y flex;
                                      apt-get install -y bison;
-									 apt-get install -y pkg-config;
-									 apt-get install -y libxcb-keysyms1-dev;
+				     apt-get install -y pkg-config;
+				     apt-get install -y libxcb-keysyms1-dev;
                                      apt-get install -y libvulkan-dev;
-									 wget https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/spirv-headers/1.3-1/spirv-headers_1.3.orig.tar.gz ;
-									 tar -zxvf spirv-headers_1.3.orig.tar.gz;
-									 mv spirv-headers-1.3/include/spirv /usr/include"
+				     wget https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/spirv-headers/1.3-1/spirv-headers_1.3.orig.tar.gz ;
+				     tar -zxvf spirv-headers_1.3.orig.tar.gz;
+				     mv spirv-headers-1.3/include/spirv /usr/include"
